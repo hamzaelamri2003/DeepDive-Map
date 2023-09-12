35c4c775df0d9a26e048af34c601722c4d3bb647
Collin Molenaar <collin1911@live.nl>
2023-09-12 14:44:59 +0200

    x

---
+++ database.sql
@@ -0,0 +1,22 @@
+/*
+gegevens die in de database moeten:
+
+location
+id
+coordinaten (x, y)
+vies (0, 1, 2)
+claimed (true/false)
+
+cleaning crew
+id
+members
+active
+currently working on (location[id])
+
+backlogs
+id
+cleaning crew id
+location id
+time of report
+report
+*/
